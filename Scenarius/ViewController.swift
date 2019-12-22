b7870978d49fd566b4756ea74fda7cb53998a84c
GrSkiy <43372592+GrSkiy@users.noreply.github.com>
2019-12-22 19:16:20 +0300

    Initial Commit

---
+++ Scenarius/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scenarius
+//
+//  Created by Gr.Skiy on 22/12/2019.
+//  Copyright © 2019 Gr.Skiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
